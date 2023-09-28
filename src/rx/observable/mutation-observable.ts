bd403d5965ee0f1de74e76d89ef827f0628ab1cc
Nikola Svitlica a.k.a TheCelavi <thecelavi@gmail.com>
2023-09-28 10:55:03 +0200

    WIP

--- src/rx/observable/mutation-observable.ts
+++ src/rx/observable/mutation-observable.ts
@@ -1,10 +1,11 @@
 import {
     Observable,
     Subscriber,
+    TeardownLogic,
 } from 'rxjs';
 
 export function mutationObservable(target: Node, options?: MutationObserverInit): Observable<MutationRecord[]> {
-    return new Observable((observer: Subscriber<MutationRecord[]>) => {
+    return new Observable((observer: Subscriber<MutationRecord[]>): TeardownLogic => {
         let mutation: MutationObserver = new MutationObserver((mutations: MutationRecord[]): void => {
             observer.next(mutations);
         });
